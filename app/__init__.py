223127a8a4707a36fb114dc8bf37b59c74c6ff6b
EitanKats <eitan.kats96@gmail.com>
2018-10-16 22:57:23 +0300

    minor fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify, abort
 
 from config import app_config
 
@@ -31,9 +31,7 @@ def app_factory(config_name):
             message = messages.pop()
             return message
         except IndexError:
-            response = jsonify({"error": "no messages left"})
-            response.status_code = 404
-            return response
+            abort(404, {"error": "no messages left"})
 
     def send_message_to_neighbour(message):
         try:
@@ -41,5 +39,6 @@ def app_factory(config_name):
             r = requests.post(app.config['BACKUP_HOST'], data=message, params=payload, timeout=10)
         except Exception as e:
             app.logger.error("could not send to backup host reason: {0}".format(e))
+            abort(503, error='Backup server unavailable')
 
     return app
